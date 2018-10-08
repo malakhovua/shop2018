165a47e59a737c8ba5c22712b79badfc4d9d824d
Malakhov A <malakhovua@gmail.com>
2018-10-08 22:52:25 +0300

    08102018

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -28,11 +28,16 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
     @order = Order.new(order_params)
+    @order.add_line_items_from_cart(@cart)
 
     respond_to do |format|
       if @order.save
-        format.html { redirect_to @order, notice: 'Order was successfully created.' }
-        format.json { render :show, status: :created, location: @order }
+        Cart.destroy(session[:cart_id])
+        session[:cart_id] = nil
+        format.html { redirect_to store_index_url, notice: 
+          'Thank you for you order.' }
+        format.json { render :show, status: :created,
+           location: @order }
       else
         format.html { render :new }
         format.json { render json: @order.errors, status: :unprocessable_entity }

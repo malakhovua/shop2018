2c2c560fdf3ab446f9c11c1014670f2386d40eaa
Malakhov A <malakhovua@gmail.com>
2018-10-14 22:46:37 +0300

    14102018

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,15 @@
 class Order < ApplicationRecord
     has_many :line_items, dependent: :destroy
+   
     enum pay_type: {
         "Check"              => 0,
         "Credit card"        => 1,
         "Purchase order"     => 2 
     }
- validates :name, :address, :email, presence: true
- validates :pay_type, inclusion: pay_types.keys   
+
+    validates :name, :address, :e_mail, presence: true
+    validates :pay_type, inclusion: pay_types.keys   
+
 
  def add_line_items_from_cart (cart)
      cart.line_items.each do |item|

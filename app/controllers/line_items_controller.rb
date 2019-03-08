50c5198d7e7f316d6eaf9a9a15a9f883e75e81b5
alex <malakhovua@GMAIL.COM>
2019-03-08 14:05:45 +0200

    080819_3

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -60,10 +60,10 @@ class LineItemsController < ApplicationController
     respond_to do |format|
       if @line_item.save
         #format.html {redirect_to new_orders_url}
-        # format.html { redirect_to @line_item.cart}
+        #format.html { redirect_to products_url }
         format.js
-        #format.json {render :show,
-         #                   status: :created, location: @line_item}
+        format.json {render :show,
+                            status: :created, location: @line_item}
       else
         format.html {render :new}
         format.json {render json: @line_item.errors, status: :unprocessable_entity}
